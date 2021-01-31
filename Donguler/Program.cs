bbe785b04f2ac7c14e2b5abc91af04181a627c20
AyseTa <ayseta@windowslive.com>
2021-01-31 16:52:09 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlamaya Başlangıç için Temel Kurs";
+            string kurs3 = "Java";
+
+            //Console.WriteLine(kurs1);
+            //Console.WriteLine(kurs2);
+            //Console.WriteLine(kurs3); 
+            // bu statik programlama yani mecburen her değişkeni tek tek çağırıp her değişikliği tek tek uygulamak gerekir.istemeyiz.
+
+            
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı", "Programlamaya Başlangıç için Temel Kurs", "Java" };
+            // gerçek hayatta bu bilgi bir veri kaynağından geliyor ve biz sadece atıyorum kurslarıGetir(); yazıyoruz.Dinamikolan. 
+
+            for (int i = 0; kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Sayfa sonu-footer");
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        public void Ekle (Urun urun) 
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi: "+ urun.Adi);
+        }
+
+        //böyle yapmamalıyız  aşağıdaki gibi çünkü programda tek tek bilgi girmek gerektircek ayrıca metotda bir değişiklikte hepsi patlıyor
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int stokAdedi) 
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi: " + urunAdi);
+        }
+    }
+}
